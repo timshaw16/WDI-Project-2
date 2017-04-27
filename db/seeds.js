192c585c94c72c0f2aa29ad324ee7afcda422698
timshaw16 <tshaw2008@icloud.com>
2017-04-27 13:30:23 +0100

    added more countries

--- db/seeds.js
+++ db/seeds.js
@@ -32,11 +32,11 @@ Country
       image: 'images/.png',
       resort: [
         {
-          name: 'Kitzbühel',
+          name: 'Kitzbuhel',
           website: 'https://www.kitzbuehel.com/en',
           lat: 47.4363937,
           lng: 12.3551052,
-          description: 'Kitzbühel is a small Alpine town east of Innsbruck, in the western Austrian province of Tyrol. It\'s a fashionable winter resort, known for the annual Hahnenkamm downhill race. Upscale shops and cafes line the streets of its medieval center. Museum Kitzbühel chronicles the history of the town and the area’s winter sports, with paintings by local artist Alfons Walde. Its rooftop terrace has sweeping town views.'
+          description: 'Kitzbuhel is a small Alpine town east of Innsbruck, in the western Austrian province of Tyrol. It\'s a fashionable winter resort, known for the annual Hahnenkamm downhill race. Upscale shops and cafes line the streets of its medieval center. Museum Kitzbühel chronicles the history of the town and the area’s winter sports, with paintings by local artist Alfons Walde. Its rooftop terrace has sweeping town views.'
         }
       ]
     },
@@ -50,7 +50,21 @@ Country
           website: 'https://www.pasdelacasa.com',
           lat: 42.542445,
           lng: 1.7281864,
-          description: 'The ski resort of Pas de la Casa sits below the pass on the side closest to France, and below the Pic d\'Envalira (2,827 metres (9,275 ft)). It\'s first ski lift was opened in 1957 and it now has 31 lifts, 100 kilometres (62 mi) of pistes and 6.26 square kilometres (2 sq mi) of skiable terrain. The highest skiable point is 2,640 metres (8,661 ft). Its popularity has grown with the burgeoning ski and snowboard industry in the principality: it is the highest resort in Andorra,[citation needed] has the best snow record,[1] and is the easiest to get to from Barcelona or Toulouse airports.'
+          description: 'The ski resort of Pas de la Casa sits below the pass on the side closest to France, and below the Pic d\'Envalira (2,827 metres (9,275 ft)). It\'s first ski lift was opened in 1957 and it now has 31 lifts, 100 kilometres (62 mi) of pistes and 6.26 square kilometres (2 sq mi) of skiable terrain. The highest skiable point is 2,640 metres (8,661 ft). Its popularity has grown with the burgeoning ski and snowboard industry in the principality: it is the highest resort in Andorra has the best snow record, and is the easiest to get to from Barcelona or Toulouse airports.'
+        }
+      ]
+    },
+    {
+      region: 'Europe',
+      country: 'Bulgaria',
+      image: 'images/.png',
+      resort: [
+        {
+          name: 'Bansko',
+          website: 'http://www.bulgariaski.com/bansko',
+          lat: 41.8356983,
+          lng: 23.4694057,
+          description: 'Bansko is a town at the foot of the Pirin Mountains, which are part of Pirin National Park, in southwest Bulgaria. It’s a gateway to numerous ski and snowboard slopes on Todorka Peak, including the challenging Tomba run. Footpaths cross the park’s craggy alpine landscape, home to bears and wolves. The park is also known for the high-altitude Vihren Peak, with forested ridges and striking glacial lakes'
         }
       ]
     },
@@ -61,10 +75,10 @@ Country
       resort: [
         {
           name: 'Valmeinier',
-          website: 'http://www.valmeinier.com/uk/index-winter.aspx',
+          website: 'http://www.valmeinier.com/uk',
           lat: 45.1512076,
           lng: 6.4389113,
-          description: 'Valmeinier is a commune in the Savoie department in the Auvergne-Rhône-Alpes region in south-eastern France. Valmeinier, located at the foot of the Mont Thabor, 3207 meters, is a medium size ski resort'
+          description: 'Valmeinier is a commune in the Savoie department in the Auvergne-Rhone-Alpes region in south-eastern France. Valmeinier, located at the foot of the Mont Thabor, 3207 meters, is a medium size ski resort'
         }
       ]
     },
@@ -75,10 +89,24 @@ Country
       resort: [
         {
           name: 'Sauze d\'Oulx',
-          website: 'https://www.sauzeonline.com/',
+          website: 'https://www.sauzeonline.com',
           lat: 45.0273981,
           lng: 6.8442727,
           description: 'Sauze d\'Oulx is a town and comune in the province of Turin, Piedmont located 80 kilometres from Turin in the Val di Susa, at the foot of Monte Genevris. It was the site of the freestyle skiing events of the 2006 Olympic Winter Games.'
+        }
+      ]
+    },
+    {
+      region: 'Europe',
+      country: 'Slovenia',
+      image: 'images/.png',
+      resort: [
+        {
+          name: 'Kranjska Gora',
+          website: 'https://www.slovenia.info/en',
+          lat: 46.4604035,
+          lng: 13.700742,
+          description: 'ituated just a few miles from the Italian and the Austrian borders, the picturesque village of Kranjska Gora is set in spectacular surroundings with the majestic Julian Alps as its backdrop. The resort is ideal for first time and improving skiers who will be well cared for by one of the top ski schools. Kranjska Gora offers plenty of cross country skiing and for non skiers, there’s an excursion programme to enjoy. The relaxed ambience of the resort with its local bars, restaurants, disco, casino and above all, the friendly Slovenian people makes for a truly memorable holiday..'
         }
       ]
     },
